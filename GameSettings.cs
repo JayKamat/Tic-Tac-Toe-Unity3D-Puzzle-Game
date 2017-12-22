b35223b843789375a401b2cf0c63b397910de760
Jay Kamat <jkamat01@gmail.com>
2017-12-22 23:26:52 +0530

    Initial commit

---
+++ GameSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour {
+    
+    //**Global Variables
+    //Rows & Columns in the Game
+    public static int iMvalue;
+    public static int iNvalue;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
